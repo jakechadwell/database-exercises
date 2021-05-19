cfededc4366d1a5738224f394c7f9f3f43097930
Jake Chadwell <jake.chadwell01@gmail.com>
2021-05-19 10:11:59 -0500

    select exercises complete

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,3 +1,16 @@
 USE codeup_test_db;
 
+SELECT 'Album titles by Pink Floyd' AS 'Info';
 SELECT name FROM albums WHERE artist = 'Pink Floyd';
+SELECT 'Year Sgt. Pepper''s Lonely Hearts Club Band was released' AS 'Info';
+SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+SELECT 'Genre for Nevermind'AS 'Info';
+SELECT genre FROM albums WHERE name = 'Nevermind';
+SELECT 'Albums released in the 90''s' AS 'Info';
+SELECT name FROM albums WHERE release_date BETWEEN 1990 AND 1999;
+SELECT 'Albums with less than 20 million certified sales' AS 'Info';
+SELECT name FROM albums WHERE sales < 20.0;
+SELECT 'Rock albums' AS 'Info';
+SELECT name FROM albums WHERE genre = 'rock';
+
+
