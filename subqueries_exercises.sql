9a7e2d18a5a9b6464c4cb3fa5e7082dcee0dcc5b
Jake Chadwell <jake.chadwell01@gmail.com>
2021-05-24 10:59:06 -0500

    completed subqueries exercise

---
+++ subqueries_exercises.sql
@@ -0,0 +1,21 @@
+USE employees;
+
+SELECT * FROM employees WHERE hire_date IN (
+    SELECT hire_date
+    FROM employees
+    WHERE emp_no = 101010
+    );
+
+SELECT title FROM titles WHERE emp_no IN (
+    SELECT emp_no
+    FROM employees
+    WHERE first_name = "Aamod"
+    );
+
+SELECT first_name, last_name FROM employees WHERE emp_no IN (
+    SELECT emp_no
+    FROM dept_manager
+    WHERE to_date > current_date
+    AND gender = 'F'
+    );
+
