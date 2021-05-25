6913c33d74c12f54d2400c56a131fa2a2e7aeab3
Jake Chadwell <jake.chadwell01@gmail.com>
2021-05-24 23:52:18 -0500

    completed join exercises

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,9 +1,35 @@
 USE employees;
 
-SELECT CONCAT(e.first_name, ' ', e.last_name) AS full_name, d.dept_name
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS Department_Manager, d.dept_name
 FROM employees as e
-         JOIN dept_emp as de
-              ON de.emp_no = e.emp_no
+         JOIN dept_manager as dm
+              ON dm.emp_no = e.emp_no
          JOIN departments as d
-              ON d.dept_no = de.dept_no
-WHERE de.to_date = '9999-01-01' AND e.emp_no = 10001;
+              ON d.dept_no = dm.dept_no;
+
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS Department_Manager, d.dept_name
+FROM employees as e
+         JOIN dept_manager as dm
+              ON dm.emp_no = e.emp_no
+         JOIN departments as d
+              ON d.dept_no = dm.dept_no
+                WHERE e.gender = 'F';
+
+SELECT t.title, COUNT(title)
+FROM dept_emp as de
+JOIN titles as t
+ON t.emp_no = de.emp_no
+    JOIN departments as d
+    ON d.dept_no = de.dept_no
+WHERE d.dept_name = 'Customer Service'
+GROUP BY title;
+
+SELECT d.dept_name AS dept, CONCAT(e.first_name, ' ', e.last_name) AS manager, s.salary AS salary
+FROM employees as e
+         JOIN dept_manager as dm
+              ON dm.emp_no = e.emp_no
+         JOIN departments as d
+              ON d.dept_no = dm.dept_no
+         JOIN salaries as s
+              ON s.emp_no = e.emp_no
+WHERE dm.to_date = '9999-01-01' AND s.to_date = '9999-01-01';
--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -9,7 +9,7 @@ SELECT * FROM employees WHERE hire_date IN (
 SELECT title FROM titles WHERE emp_no IN (
     SELECT emp_no
     FROM employees
-    WHERE first_name = "Aamod"
+    WHERE first_name = 'Aamod'
     );
 
 SELECT first_name, last_name FROM employees WHERE emp_no IN (
